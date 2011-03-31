4ad276efe40830aa36a213b4f84a88331ccc3ef2
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-03-31 15:42:43 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketer::Application.config.secret_token = 'e87004e5ea185795f5a29b7444f1f2373cef8f26db248daf2cb48af56ee738164fbb69860196e5a86373e01670dc4ed069901005631c6efbf5151e9238f836f8'

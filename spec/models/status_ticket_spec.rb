afdddef47aa6761bf9da2b8e6fea0d0c31fb6265
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-04-06 17:17:34 -0300

    added new tests and remove a pendence from helper test

--- spec/models/status_ticket_spec.rb
+++ spec/models/status_ticket_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe StatusTicket do
 
   before( :each ) do
-    @statusTicket = StatusTicket.new( :value => "Opened", :acronym => "O", :user_id => 1 )
+    @statusTicket = StatusTicket.new( :value => "Opened", :acronym => "O", :user => "1" )
   end
 
   it "can be instantiated" do
@@ -26,6 +26,57 @@ describe StatusTicket do
   it "should not be an instance valid if 'acronym' property is nil" do
     @statusTicket.acronym= nil
     @statusTicket.should_not be_valid
+  end
+  
+  it "should not be an instance valid if 'user' property is nil" do
+    @statusTicket.user= nil
+    @statusTicket.should_not be_valid
+  end
+  
+  it "should not be an instance valid with length of 'acronym' equal zero" do
+    @statusTicket.acronym= ''
+    @statusTicket.should_not be_valid
+  end
+  
+  it "should not be an instance valid with length of 'acronym' greater than one" do
+    @statusTicket.acronym= 'OP'
+    @statusTicket.should_not be_valid
+  end
+  
+  it "should not be an instance valid with length of 'value' equal zero" do
+    @statusTicket.value= ''
+    @statusTicket.should_not be_valid
+  end
+
+  it "should be an instance valid with length of 'value' between one and eighty" do
+    @statusTicket.value= 'This is a big text to test a validate for the length of this property.'
+    @statusTicket.should be_valid
+  end
+
+  it "should not be an instance valid with length of 'value' greater than eighty" do
+    @statusTicket.value= 'This is a big text to test a validate for the length of this property. If greater this instance is not valid.'
+    @statusTicket.should_not be_valid
+  end
+
+  it "should not be create a register with an existing 'value' and 'acronym' registered" do
+    @statusTicket.save
+
+    @statusInvalid = StatusTicket.new( :value => "Opened", :acronym => "O", :user => "1" )
+    @statusInvalid.save.should == false
+  end
+
+  it "should not be create a register with an existing 'value' registered" do
+    @statusTicket.save
+
+    @statusInvalid = StatusTicket.new( :value => "Opened", :acronym => "P", :user => "1" )
+    @statusInvalid.save.should == false
+  end
+
+  it "should not be create a register with an existing 'acronym' registered" do
+    @statusTicket.save
+
+    @statusInvalid = StatusTicket.new( :value => "Openeds", :acronym => "O", :user => "1" )
+    @statusInvalid.save.should == false
   end
 
 end

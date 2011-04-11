d36a98eb39f62c1782406a1a9e9c3ff254849518
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-04-10 21:51:13 -0300

    correct the symbol called 'length'

--- db/migrate/20110405135246_create_status_tickets.rb
+++ db/migrate/20110405135246_create_status_tickets.rb
@@ -1,8 +1,8 @@
 class CreateStatusTickets < ActiveRecord::Migration
   def self.up
     create_table :status_tickets do |t|
-      t.string :value, :lenght => 80
-      t.string :acronym, :lenght => 1
+      t.string :value, :length => 80
+      t.string :acronym, :length => 1
       t.string :user
 
       t.timestamps

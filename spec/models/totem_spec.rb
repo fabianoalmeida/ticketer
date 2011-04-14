d60378f7696d1ccac1bcbdf60f13c5c25ab516e5
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-04-14 16:20:23 -0300

    modified some tests and correcting some errors

--- spec/models/totem_spec.rb
+++ spec/models/totem_spec.rb
@@ -4,12 +4,10 @@ describe Totem do
 
   before( :each ) do
     @status = stub_model( Status, :value => "Active" )
+    
+    @place = stub_model( Place )
 
-    @totem = Totem.new( :value => "Totem 01", :ip => "10.32.1.22", :status => @status, :user => "1" )
-  end
-
-  it "can be an instance" do
-    @totem.should be_an_instance_of( Totem )
+    @totem = Totem.new( :value => "Totem 01", :ip => "10.32.1.22", :status => @status, :place => @place, :user => "1" )
   end
 
   it "can be save successfully" do
@@ -18,12 +16,12 @@ describe Totem do
 
   it "should not be an instanve valid if the property 'value' is nil" do
     @totem.value= nil
-    @totem.should_not be_valid
+    @totem.should_not be_valid( :value )
   end
 
   it "should not be an instance valid if the length of 'value' property is equal zero" do
     @totem.value= ''
-    @totem.should_not be_valid
+    @totem.should_not be_valid( :value )
   end
 
   it "should be an instance valid if the length of 'value' property is between one and eighty" do
@@ -38,53 +36,68 @@ describe Totem do
 
   it "should not be an instanve valid if the property 'ip' is nil" do
     @totem.ip= nil
-    @totem.should_not be_valid
+    @totem.should_not be_valid( :ip )
   end
 
   it "should not be an instance valid if the length of 'ip' property is equal zero" do
     @totem.ip= ''
-    @totem.should_not be_valid
+    @totem.should_not be_valid( :ip )
   end
 
   it "should be an instance valid if the length of 'ip' property is between one and fifteen" do
-    @totem.ip= '255.255.255.255'
+    @totem.ip= "10.32.2.25"
     @totem.should be_valid
   end
 
   it "should not be an instance valid if the length of 'ip' property is greater than fifteen" do
     @totem.ip= '255.255.255.255:1521'
-    @totem.should_not be_valid
+    @totem.should_not be_valid( :ip )
+  end
+
+  it "should not be an instance valid if the value of 'ip' property is not within the correct format" do
+    @totem.ip= "32.2.25"
+    @totem.should_not be_valid( :ip )
   end
 
   it "should not permit to create a new register with an existing 'value' registered" do
     @totem.save
 
-    @totem_invalid = Totem.new( :value => "Totem 01", :ip => "10.32.1.22", :status => @status, :user => "1" )
-    @totem_invalid.save.should == false
+    @totem_invalid = Totem.new( :value => "Totem 01", :ip => "10.32.1.22", :status => @status, :place => @place, :user => "1" )
+    @totem_invalid.save 
+    @totem_invalid.errors[ :value ].should == [ "already exists!" ]
+  end
+
+  it "should not permit to create a new register with an existing 'value' registered even that has a different case sensitive" do
+    @totem.save
+
+    @totem_invalid = Totem.new( :value => "ToTeM 01", :ip => "10.32.1.22", :status => @status, :place => @place, :user => "1" )
+    @totem_invalid.save 
+    @totem_invalid.errors[ :value ].should == [ "already exists!" ]
   end
 
   it "should permit to create a new register with an unexisting 'value' registered" do
     @totem.save
 
-    @totem_valid = Totem.new( :value => "Totem 02", :ip => "10.32.1.2", :status => @status, :user => "1" )
+    @totem_valid = Totem.new( :value => "Totem 02", :ip => "10.32.1.2", :status => @status, :place => @place, :user => "1" )
     @totem_valid.save.should == true
   end
 
   it "should not permit to create a new register with an existing 'ip' registered" do
     @totem.save
 
-    @totem_invalid = Totem.new( :value => "Totem 03", :ip => "10.32.1.22", :status => @status, :user => "1" )
-    @totem_invalid.save.should == false
+    @totem_invalid = Totem.new( :value => "Totem 03", :ip => "10.32.1.22", :status => @status, :place => @place, :user => "1" )
+    @totem_invalid.save
+    @totem_invalid.errors[ :ip ].should == [ "already exists!" ]
   end
 
   it "should permit to create a new register with an unexisting 'ip' registered" do
     @totem.save
 
-    @totem_valid = Totem.new( :value => "Totem 04", :ip => "10.32.1.21", :status => @status, :user => "1" )
+    @totem_valid = Totem.new( :value => "Totem 04", :ip => "10.32.1.20", :status => @status, :place => @place, :user => "1" )
     @totem_valid.save.should == true
   end
 
-  it "should not be an instanve valid if the property 'user' is nil" do
+  it "should not be an instance valid if the property 'user' is nil" do
     @totem.user= nil
     @totem.should_not be_valid
   end
@@ -94,13 +107,22 @@ describe Totem do
     @totem.should_not be_valid
   end
 
-  it "should not be an instanve valid if the property 'status' is nil" do
+  it "should not be an instance valid if the property 'status' is nil" do
     @totem.status= nil
     @totem.should_not be_valid
   end
 
   it "Status should be the same value of property :status" do
     @totem.status.should eq( @status )
+  end
+
+  it "should not be an instance valid if the property 'place' is nil" do
+    @totem.place= nil
+    @totem.should_not be_valid
+  end
+
+  it "Status should be the same value of property :place" do
+    @totem.place.should eq( @place )
   end
 
 end

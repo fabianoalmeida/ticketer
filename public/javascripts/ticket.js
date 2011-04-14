7ec97333743a0d6e4d5588b53211ebf355544f55
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-04-13 23:18:29 -0300

    removed a jQuery block that submitted a request from a select tag

--- public/javascripts/ticket.js
+++ public/javascripts/ticket.js
@@ -1,10 +1,5 @@
 jQuery( document ).ready( function() {
   jQuery( 'a' ).click( function() { 
-        jQuery( '#ticket_ticket_type' ).val( this.id );
+        jQuery( '#ticket_ticket_type_id' ).val( this.id );
         jQuery( '#new_ticket' ).submit();
       } );
-  
-  jQuery( 'select' ).change( function() { 
-        jQuery( '#new_ticket' ).submit();
-      } );
-  } )

35bd7adc01a5d2b9bbd93df8dbc7db084ecf8743
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-04-14 16:17:24 -0300

    added a format to ip attribute

--- app/models/totem.rb
+++ app/models/totem.rb
@@ -19,6 +19,7 @@ class Totem < ActiveRecord::Base
   validates :ip,
     :length => { :in => 1..15 },
     :uniqueness => { :message => "already exists!" },
+    :format => { :with => /^((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))(?![\d])$/ },
     :allow_blank => false
 
 end

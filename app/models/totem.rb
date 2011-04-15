baa59bd63dd13c20cbecfb8e4eef1891aa2a4237
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-04-15 15:46:02 -0300

    fixed when a format's message will appear

--- app/models/totem.rb
+++ app/models/totem.rb
@@ -14,12 +14,12 @@ class Totem < ActiveRecord::Base
   validates :value,
     :length => { :in => 1..80 },
     :uniqueness => { :message => "already exists!", :case_sensitive => false },
-    :allow_blank => false
+    :allow_blank => true
 
   validates :ip,
     :length => { :in => 1..15 },
     :uniqueness => { :message => "already exists!" },
     :format => { :with => /^((?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?))(?![\d])$/ },
-    :allow_blank => false
+    :allow_blank => true
 
 end

8a7427c4979ec1785e13931661aa5c2f89a2ca8a
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-04-16 21:05:45 -0300

    fixed to create a new panel just with a place

--- app/controllers/panels_controller.rb
+++ app/controllers/panels_controller.rb
@@ -2,7 +2,7 @@ class PanelsController < ApplicationController
   # GET /panels
   # GET /panels.xml
   def index
-    @panels = Panel.all
+    @panels = Panel.where(:place_id => params[:place_id])
 
     respond_to do |format|
       format.html # index.html.erb
@@ -25,6 +25,7 @@ class PanelsController < ApplicationController
   # GET /panels/new.xml
   def new
     @panel = Panel.new
+    @place = Place.find(params[:place_id])
 
     respond_to do |format|
       format.html # new.html.erb
@@ -35,19 +36,22 @@ class PanelsController < ApplicationController
   # GET /panels/1/edit
   def edit
     @panel = Panel.find(params[:id])
+    @place = Place.find(params[:place_id])
   end
 
   # POST /panels
   # POST /panels.xml
   def create
     @panel = Panel.new(params[:panel])
+    @place = Place.find(params[:place_id])
+    @panel.user = "user test"
 
     respond_to do |format|
       if @panel.save
-        format.html { redirect_to(@panel, :notice => 'Panel was successfully created.') }
+        format.html { redirect_to(place_panel_url(@panel.place, @panel), :notice => 'Panel was successfully created.') }
         format.xml  { render :xml => @panel, :status => :created, :location => @panel }
       else
-        format.html { render :action => "new" }
+        format.html { render :action => :new }
         format.xml  { render :xml => @panel.errors, :status => :unprocessable_entity }
       end
     end
@@ -57,10 +61,11 @@ class PanelsController < ApplicationController
   # PUT /panels/1.xml
   def update
     @panel = Panel.find(params[:id])
+    @place = Place.find(params[:place_id])
 
     respond_to do |format|
       if @panel.update_attributes(params[:panel])
-        format.html { redirect_to(@panel, :notice => 'Panel was successfully updated.') }
+        format.html { redirect_to(place_panel_url(@panel.place, @place), :notice => 'Panel was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }

cdebf8b8487377bb13df3b8b4de6aa1da77de6c1
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-04-18 07:35:47 -0300

    fixed tests

--- spec/views/wickets/new.html.erb_spec.rb
+++ spec/views/wickets/new.html.erb_spec.rb
@@ -4,8 +4,8 @@ describe "wickets/new.html.erb" do
   before(:each) do
     assign(:wicket, stub_model(Wicket,
       :value => "MyString",
-      :guidance_id => 1,
-      :status_id => 1,
+      :guidance => stub_model(Guidance),
+      :status => stub_model(Status),
       :user => "MyString"
     ).as_new_record)
   end
@@ -16,8 +16,8 @@ describe "wickets/new.html.erb" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => wickets_path, :method => "post" do
       assert_select "input#wicket_value", :name => "wicket[value]"
-      assert_select "input#wicket_guidance_id", :name => "wicket[guidance_id]"
-      assert_select "input#wicket_status_id", :name => "wicket[status_id]"
+      assert_select "input#wicket_guidance", :name => "wicket[guidance]"
+      assert_select "input#wicket_status", :name => "wicket[status]"
       assert_select "input#wicket_user", :name => "wicket[user]"
     end
   end

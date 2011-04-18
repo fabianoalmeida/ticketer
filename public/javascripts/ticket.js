48cafbb79e9db28fc1da806f2ecb28235ea0461f
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-04-18 17:26:05 -0300

    added a new jQuery submit from a ticket to create a new call history

--- public/javascripts/ticket.js
+++ public/javascripts/ticket.js
@@ -1,6 +1,13 @@
 jQuery( document ).ready( function() {
   jQuery( 'a' ).click( function() { 
-        jQuery( '#ticket_ticket_type_id' ).val( this.id );
-        jQuery( '#new_ticket' ).submit();
+        if ( jQuery( '#ticket_ticket_type_id' ) != null ) {
+          jQuery( '#ticket_ticket_type_id' ).val( this.id );
+          jQuery( '#new_ticket' ).submit();
+        }
+        
+        if ( jQuery( '#call_history_ticket_id' ) != null ) {
+          jQuery( '#call_history_ticket_id' ).val( this.id );
+          jQuery( '#new_call_history' ).submit();
+        }
       } );
   } )

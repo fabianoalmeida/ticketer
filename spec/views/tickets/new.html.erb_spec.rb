d597664a5ca477fc04eca51b51c2aa2eb8076ac5
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-04-19 00:03:56 -0300

    removed useless lines

--- spec/views/tickets/new.html.erb_spec.rb
+++ spec/views/tickets/new.html.erb_spec.rb
@@ -4,9 +4,9 @@ describe "tickets/new.html.erb" do
   before(:each) do
     assign(:ticket, mock_model(Ticket,
       :value => "MyString",
-      :ticket_type_id => 1,
-      :status_ticket_id => 1,
-      :place_id => 1
+      :ticket_type => stub_model(TicketType),
+      :status_ticket => stub_model(StatusTicket),
+      :place => stub_model(Place)
     ).as_new_record)
   end
 
@@ -16,8 +16,7 @@ describe "tickets/new.html.erb" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => tickets_path, :method => "post" do
       assert_select "input#ticket_value", :name => "ticket[value]"
-      assert_select "input#ticket_ticket_type_id", :name => "ticket[ticket_type_id]"
-      assert_select "input#ticket_status_ticket_id", :name => "ticket[status_ticket_id]"
+      assert_select "input#ticket_ticket_type", :name => "ticket[ticket_type]"
     end
   end
 end

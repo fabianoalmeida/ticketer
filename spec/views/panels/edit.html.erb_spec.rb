638a227d81d4b6e2345571749d5e4421a851f50f
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-04-19 17:32:34 -0300

    More broken test fixed

--- spec/views/panels/edit.html.erb_spec.rb
+++ spec/views/panels/edit.html.erb_spec.rb
@@ -7,7 +7,6 @@ describe "panels/edit.html.erb" do
       :value => "MyString",
       :ip => "MyString",
       :status_id => 1,
-      :user => "MyString",
       :place => @place
     ))
   end
@@ -19,7 +18,6 @@ describe "panels/edit.html.erb" do
       assert_select "input#panel_value", :name => "panel[value]"
       assert_select "input#panel_ip", :name => "panel[ip]"
       #assert_select "input#panel_status_id", :name => "panel[status_id]"
-      assert_select "input#panel_user", :name => "panel[user]"
     end
   end
 end
--- spec/views/statuses/index.html.erb_spec.rb
+++ spec/views/statuses/index.html.erb_spec.rb
@@ -5,11 +5,16 @@ describe "statuses/index.html.erb" do
     assign(:statuses, [
       stub_model(Status,
         :value => "Value",
-        :user => 1
+        :user => 1,
+        :status => stub_model(Status, :value => "value"),
+        :created_at => DateTime.now
       ),
       stub_model(Status,
         :value => "Value",
-        :user => 1
+        :user => 1,
+        :status => stub_model(Status, :value => "value"),
+        :created_at => DateTime.now
+        
       )
     ])
   end
@@ -17,7 +22,7 @@ describe "statuses/index.html.erb" do
   it "renders a list of statuses" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Value".to_s, :count => 2
+    assert_select "tr>td", :text => "en, model, status, value".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => 1.to_s, :count => 2
   end

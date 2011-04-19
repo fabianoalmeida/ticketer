6be6c86f4f2605ea915c5251eaec7cbb46b011f7
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-04-19 16:50:19 -0300

    Comment temporally to development

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   #This solution looks like a kinda weird, should be improved it
   #before_filter RubyCAS::Filter, :unless => :require_been_loged?
 
+
   protect_from_forgery
 
   before_filter :set_locale

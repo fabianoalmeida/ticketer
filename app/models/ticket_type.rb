114c3266ebfe459ec3a27c0a5e0d5b2d3a185ff6
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-04-19 20:53:22 -0300

    modified the range of length for value attribute and removed the message validates cause the i18n gem

--- app/models/ticket_type.rb
+++ app/models/ticket_type.rb
@@ -8,16 +8,16 @@ class TicketType < ActiveRecord::Base
   belongs_to :status
   has_many :tickets
 
-  validates :value, :acronym, :user, :status, :presence => { :message => "is required!" }
+  validates :value, :acronym, :user, :status, :presence => true
 
   validates :value, 
-    :length => { :minimum => 1, :maximum => 80 },
-    :uniqueness => { :message => "already exists!", :case_sensitive => false },
+    :length => { :in => 1..80 },
+    :uniqueness => { :case_sensitive => false },
     :allow_blank => false
 
   validates :acronym, 
     :length => { :maximum => 3 },
-    :uniqueness => { :message => "already exists!", :case_sensitive => false },
+    :uniqueness => { :case_sensitive => false },
     :allow_blank => false
 
 end

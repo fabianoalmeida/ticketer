82d1872446ad6f98461349b355dd958d1451632c
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-04-19 15:46:26 -0300

    modified to improve, cause Ticketer has been made for Brazilians, so it was modified to support locale equal pt-BR

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  helper :all
+
   protect_from_forgery
+
+  before_filter :set_locale
+
+  private
+
+  def set_locale
+    I18n.locale = 'pt-BR'
+  end
 end

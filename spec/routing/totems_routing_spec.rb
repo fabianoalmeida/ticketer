f0a47b42c088c209398b43825b80266299b59b9d
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-04-19 00:02:07 -0300

    modified to support the rule -> create a new totem via a place

--- spec/routing/totems_routing_spec.rb
+++ spec/routing/totems_routing_spec.rb
@@ -4,31 +4,31 @@ describe TotemsController do
   describe "routing" do
 
     it "recognizes and generates #index" do
-      { :get => "/totems" }.should route_to(:controller => "totems", :action => "index")
+      { :get => "/places/1/totems" }.should route_to(:place_id => "1", :controller => "totems", :action => "index")
     end
 
     it "recognizes and generates #new" do
-      { :get => "/totems/new" }.should route_to(:controller => "totems", :action => "new")
+      { :get => "/places/1/totems/new" }.should route_to(:place_id => "1", :controller => "totems", :action => "new")
     end
 
     it "recognizes and generates #show" do
-      { :get => "/totems/1" }.should route_to(:controller => "totems", :action => "show", :id => "1")
+      { :get => "/places/1/totems/1" }.should route_to(:place_id => "1", :controller => "totems", :action => "show", :id => "1")
     end
 
     it "recognizes and generates #edit" do
-      { :get => "/totems/1/edit" }.should route_to(:controller => "totems", :action => "edit", :id => "1")
+      { :get => "/places/1/totems/1/edit" }.should route_to(:place_id => "1", :controller => "totems", :action => "edit", :id => "1")
     end
 
     it "recognizes and generates #create" do
-      { :post => "/totems" }.should route_to(:controller => "totems", :action => "create")
+      { :post => "/places/1/totems" }.should route_to(:place_id => "1", :controller => "totems", :action => "create")
     end
 
     it "recognizes and generates #update" do
-      { :put => "/totems/1" }.should route_to(:controller => "totems", :action => "update", :id => "1")
+      { :put => "/places/1/totems/1" }.should route_to(:place_id => "1", :controller => "totems", :action => "update", :id => "1")
     end
 
     it "recognizes and generates #destroy" do
-      { :delete => "/totems/1" }.should route_to(:controller => "totems", :action => "destroy", :id => "1")
+      { :delete => "/places/1/totems/1" }.should route_to(:place_id => "1", :controller => "totems", :action => "destroy", :id => "1")
     end
 
   end

9fd2d2dd35daeb40390a92cf66b92b2cf7270136
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-04-22 21:22:02 -0300

    redefined all of routes to facility of the understand by brazilian guys

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,27 @@
 Ticketer::Application.routes.draw do
-  resources :locals, :call_histories, :guidances, :call_history_types, 
-            :tickets, :ticket_types, :statuses, :status_tickets
+  
+  resources :locals, :path => 'locais', :path_names => { :new => 'novo', :show => 'visualizar', :edit => 'editar' }
+  
+  resources :call_histories, :path => 'historico_de_chamadas', :path_names => { :new => 'novo', :show => 'visualizar', :edit => 'editar' }
+  
+  resources :guidances, :path => 'orientacoes', :path_names => { :new => 'novo', :edit => 'editar' }
+  
+  resources :tickets, :path => 'senhas', :path_names => { :new => 'novo', :show => 'visualizar', :edit => 'editar' }
+  
+  resources :ticket_types, :path => 'tipos_de_senha', :path_names => { :new => 'novo', :show => 'visualizar', :edit => 'editar' }
+  
+  resources :statuses, :path => 'status', :path_names => { :new => 'novo', :show => 'visualizar', :edit => 'editar' }
+  
+  resources :status_tickets, :path => 'status_de_senha', :path_names => { :new => 'novo', :show => 'visualizar', :edit => 'editar' }
 
-  resources :places do
+  resources :places, :path => 'localidades', :path_names => { :new => 'novo', :show => 'visualizar', :edit => 'editar' } do
 
-    resources :panels
-    resources :wickets do 
-      get 'tickets'
+    resources :panels, :path => 'paineis', :path_names => { :new => 'novo', :show => 'visualizar', :edit => 'editar' }
+    resources :wickets, :path => 'guiches', :path_names => { :new => 'novo', :show => 'visualizar', :edit => 'editar' } do 
+      get 'tickets', :path => 'senhas'
     end
     resources :totems do
-      get 'generate_ticket'
+      get 'generate_ticket', :path => 'gerar_senha'
     end
 
   end

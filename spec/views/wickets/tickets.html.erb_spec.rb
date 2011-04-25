aeb156a6dd8706bb112db0e8857827cb058ab12d
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-04-25 17:06:56 -0300

    Views tickets Wicket fucntion to put ticket waiting

---
+++ spec/views/wickets/tickets.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+describe "wickets/tickets.html.erb" do
+  before(:each) do
+    @place = assign(:place, stub_model(Place, :id => "1"))
+    assign(:tickets, [
+      stub_model(Ticket, :value => "P01" ),
+      stub_model(Wicket, :value => "P01" )
+    ])
+    assign(:tickets_waiting,[
+      stub_model(Ticket, :value => "P02" ),
+      stub_model(Wicket, :value => "P02" )
+    ])
+  end
+
+  it "renders a list of wickets" do
+    render
+    assert_select "table#tickets>tr>td", :text => "P01".to_s, :count => 2
+    assert_select "table#tickets_waiting>tr>td", :text => "P02".to_s, :count => 2
+  end
+end

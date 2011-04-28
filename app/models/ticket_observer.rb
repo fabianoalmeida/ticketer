150d25800839d7cac5c0487dd39aed45fbbd53ad
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-04-28 10:37:23 -0300

    Observer to publish create of a new Ticket

--- app/models/ticket_observer.rb
+++ app/models/ticket_observer.rb
@@ -1,12 +1,20 @@
 class TicketObserver < ActiveRecord::Observer
   
   def before_save(ticket)
-
    unless ticket.new_record? 
       state_object =  StatusTicket.id_for(ticket.state)
       ticket.status_ticket = state_object unless ticket.status_ticket.eql?(state_object)
     end
-   #here goes the Juggernaut#publish to notify a new ticket available
+    emit_ticket(:availables, ticket) 
   end
 
+  private 
+  #This should be moved to a module 
+  def emit_ticket(channel, ticket)
+    Juggernaut.publish(channel, {
+          :value => ticket.value, 
+          :id => ticket.id,
+          :time => ticket.updated_at
+        })
+  end
  end

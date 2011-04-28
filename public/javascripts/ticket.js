8e37265eb4277021eca4d0d8c5daceaad98df6f5
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-04-28 18:12:14 -0300

    added ajax invocations

--- public/javascripts/ticket.js
+++ public/javascripts/ticket.js
@@ -1,13 +1,46 @@
 jQuery( document ).ready( function() {
-  jQuery( 'a' ).click( function() { 
-        if ( jQuery( '#ticket_ticket_type_id' ) != null ) {
-          jQuery( '#ticket_ticket_type_id' ).val( this.id );
-          jQuery( '#new_ticket' ).submit();
+  jQuery( 'a.generate' ).click( function() { 
+    jQuery( '#ticket_ticket_type_id' ).val( this.id );
+    param = jQuery( '#new_ticket' ).serialize();
+    jQuery.ajax({
+      url: "/senhas",
+      type: "post",
+      data: param,
+      success: function(data){
+        ticket = data.ticket;
+        alert('Sua senha e: ' + ticket.value);
+      }
+      })
+    } );
+  jQuery('input#call_next').click( function() {
+    jQuery.ajax({
+        url: "chamar_proximo",
+        type: "get",
+        success: function(data){
+          ticket = data.ticket;
+          $("#tickets_called").append('<tr><td>'+ticket.value+'</td><td>'+ticket.updated_at+'</td></tr>');
+          $('input#current').val(ticket.id);
         }
-        
-        if ( jQuery( '#call_history_ticket_id' ) != null ) {
-          jQuery( '#call_history_ticket_id' ).val( this.id );
-          jQuery( '#new_call_history' ).submit();
+      })
+    })
+  jQuery('input#pending').click( function() {
+    currentParameter = 'ticket_id='+$('input#current').val();
+    jQuery.ajax({
+        url: "por_em_espera",
+        type: "put",
+        data: currentParameter,
+        success: function(data){
+          ticket = data.ticket;
+          $("#tickets_waiting").append('<tr><td>'+ticket.value+'</td><td>'+ticket.updated_at+'</td></tr>');
         }
-      } );
-  } )
+      })
+    })
+  jQuery('input#recall').click( function() {
+    currentParameter = 'ticket_id='+$('input#current').val();
+    jQuery.ajax({
+        url: "rechamar",
+        type: "put",
+        data: currentParameter,
+      })
+    })
+   } )

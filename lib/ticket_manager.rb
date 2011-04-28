ca9c170cea25d6f642f04fa1e9e3542facd686ee
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-04-28 18:08:26 -0300

    created a method that return all registers by day called 'today', and put this method in all queries

--- lib/ticket_manager.rb
+++ lib/ticket_manager.rb
@@ -8,24 +8,23 @@ module TicketManager
 
       place= place.id if place.is_a? Place
 
-      next_ticket = Ticket.where(:place_id => place, 
-                                 :ticket_type_id => TicketType.preferencial.id, 
-                                 :status_ticket_id => StatusTicket.available.id, 
-                                 :created_at => Date.today.midnight...Date.tomorrow.midnight).order(:created_at).first
+      next_ticket = where(:place_id => place, :ticket_type_id => TicketType.preferencial.id, :status_ticket_id => StatusTicket.available.id).today.order(:created_at).first
 
-
-      next_ticket ||= Ticket.where(:place_id => place, 
-                                   :status_ticket_id => StatusTicket.available.id, 
-                                   :created_at => Date.today.midnight...Date.tomorrow.midnight).order(:created_at).first
+      next_ticket ||= where(:place_id => place, :status_ticket_id => StatusTicket.available.id).today.order(:created_at).first
 
       next_ticket
+    end
+
+    def today
+      #TODO Move to module
+      where(:created_at => Date.today.midnight...Date.tomorrow.midnight)
     end
 
     def available_for_place(place)
 
       place= place.id if place.is_a? Place
 
-      tickets = Ticket.where(:place_id => place, :status_ticket_id => StatusTicket.available.id)
+      tickets = where(:place_id => place, :status_ticket_id => StatusTicket.available.id).today
 
       tickets
     end
@@ -34,7 +33,7 @@ module TicketManager
 
       place= place.id if place.is_a? Place
 
-      tickets = Ticket.where(:place_id => place, :status_ticket_id => StatusTicket.called.id)
+      tickets = where(:place_id => place, :status_ticket_id => StatusTicket.called.id).today
 
       tickets
     end

b4921f545eed4006d78a451ed2a116573259e864
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-05-03 14:51:55 -0300

    Refactoring for better legibility

--- lib/ticket_manager.rb
+++ lib/ticket_manager.rb
@@ -1,18 +1,14 @@
 module TicketManager 
   def self.included(base) 
-    base.send(:extend, TicketManagerClass )
+    base.extend InstanceMethods 
   end
-
-  module TicketManagerClass
+  
+  module InstanceMethods
     def next_to(place)
-
       place= place.id if place.is_a? Place
-
       next_ticket = where(:place_id => place, :ticket_type_id => TicketType.preferencial.id, :status_ticket_id => StatusTicket.available.id).today.order(:created_at).first
-
       next_ticket ||= where(:place_id => place, :status_ticket_id => StatusTicket.available.id).today.order(:created_at).first
-
       next_ticket
     end
-   end
+  end
 end

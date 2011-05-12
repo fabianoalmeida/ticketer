614d6a35a48005c9a205550d3a20e4419f78203c
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-05-12 16:30:14 -0300

    fixed errors to use today method in all the ticketer app

--- lib/ticketer.rb
+++ lib/ticketer.rb
@@ -1,6 +1,6 @@
 module Ticketer
-  class ActiveRecord::Relation
-    def today
+  class ActiveRecord::Base
+    def self.today
       where(:created_at => Date.today.midnight...Date.tomorrow.midnight) 
     end
   end

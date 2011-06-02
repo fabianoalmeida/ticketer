dc18a3ab654495c912c9e0fc1bd4328f269a89d0
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-06-02 10:18:38 -0300

    Some changes to Alert juggernaut for panel, as well change the song.

--- public/javascripts/juggernaut.panel.js
+++ public/javascripts/juggernaut.panel.js
@@ -2,10 +2,12 @@ function process(data) {
   if(data.channel =="calleds"){ 
     $('div.last_ticket').html('<div style="font-size:30px; color : black; margin : 20px 0;">Guichê: '+data.wicket_value+'<br/>Tipo: '+data.ticket_type_value+'</div>'+data.value);
    var items = $('div.history_calls .item');
-   items.last().remove();
-   items.stop().animate({left: items.width()}, 'slow');
+   if(items.size() >= 9 ){
+     items.last().remove();
+   }
    $('div.history_calls .label').after('<div class="item"><div class="botton">'+data.time+'</div><div class="content">'+data.value+'</div>');
-   jQuery('audio')[0].play();
+   $.playAudio("/audios/beep_alert.mp3");
+   //jQuery('audio')[0].play();
 
     if(data.left){ 
       jQuery('img#image_left').show();

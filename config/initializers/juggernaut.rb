2360d7667c9b0e694ec05d4c0d1ff63b8e784eb2
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-06-03 09:32:05 -0300

    modified the ip to 'localhost'

--- config/initializers/juggernaut.rb
+++ config/initializers/juggernaut.rb
@@ -1,1 +1,1 @@
-ActionView::Helpers::AssetTagHelper.register_javascript_expansion :juggernaut => ["http://10.32.2.21:8080/application.js"]
+ActionView::Helpers::AssetTagHelper.register_javascript_expansion :juggernaut => ["http://localhost:8080/application.js"]

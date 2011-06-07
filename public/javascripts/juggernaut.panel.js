c9393c9031961e84325d87cb2cba591c70d3d3fd
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-06-07 09:45:56 -0300

    minor refactoring

--- public/javascripts/juggernaut.panel.js
+++ public/javascripts/juggernaut.panel.js
@@ -1,23 +1,32 @@
-function process(data) {
-  if(data.channel =="calleds"){ 
-   $('div.last_ticket').html('<div style="font-size:30px; color : black; margin : 20px 0;">Guichê: '+data.wicket_value+'<br/>Tipo: '+data.ticket_type_value+'</div>'+data.value);
+(function(window){
+  window.process = function(data) {
+    if(data.channel =="calleds"){ 
+      //Replace the main ticket from view 
+      $('div.last_ticket').html('<div style="font-size:30px; color : black; margin : 20px 0;">Guichê: '+data.wicket_value+'<br/>Tipo: '+data.ticket_type_value+'</div>'+data.value);
 
-   var items = $('div.history_calls .item');
-   if(items.size() >= 9 ){
-     items.last().remove();
-   }
+      var items = $('div.history_calls .item');
 
-   $('div.history_calls .label').after('<div class="item"><div class="botton">'+data.time+'</div><div class="content">'+data.value+'</div>');
-   $.playAudio("/audios/beep_alert.mp3");
+       //ensure maximum tickets to history are 9 (nine)  
+      if(items.size() >= 9 ){
+        items.last().remove();
+      }
 
-    if(data.left){ 
-      jQuery('img#image_left').show();
-      jQuery('img#image_right').hide();
-    }
+      //Add the new one ticket  to history
+      $('div.history_calls .label').after('<div class="item"><div class="botton">'+data.time+'</div><div class="content">'+data.value+'</div>');
 
-    if(data.right){ 
-      jQuery('img#image_right').show();
-      jQuery('img#image_left').hide();
+      //Plays songs of notification
+      $.playAudio("/audios/beep_alert.mp3");
+
+      //Display the orientation for  the wicket that call the ticket.
+      if(data.left){ 
+        jQuery('img#image_left').show();
+        jQuery('img#image_right').hide();
+      }
+
+      if(data.right){ 
+        jQuery('img#image_right').show();
+        jQuery('img#image_left').hide();
+      }
     }
   }
-}
+})(window)

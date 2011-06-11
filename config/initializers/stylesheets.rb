f7ddd2de5af9ca1e72e06df81e8ec2f9b22d0b13
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-06-10 21:35:43 -0300

    Add primiry layout with suport cross-browser

---
+++ config/initializers/stylesheets.rb
@@ -0,0 +1,3 @@
+ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :defaults => ["application"]
+ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :defaults_ie => ["application-ie-suport"]
+ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :external => ["application-external", "facebox"]

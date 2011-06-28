ea297d52e896880d7276fe1dc1efa3951e63f6d2
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-06-28 15:57:01 -0300

    Refector to display new tickets on panel

--- public/javascripts/juggernaut.panel.js
+++ public/javascripts/juggernaut.panel.js
@@ -10,21 +10,21 @@
 
       var items = $('div.history_calls .item');
 
-       //ensure maximum tickets to history are 9 (nine)  
-      if(items.size() >= 7 ){
-        items.last().remove();
-      }
-
       //Add the new one ticket  to history
       new_item = $('<div/>').addClass("item").html(data.ticket_type_group + "<br/>" + data.value + " - " + data.wicket_value);
       tr = $("<tr/>").addClass("item");
       td_value = $("<td/>").text(data.value);
       td_wicket = $("<td/>").text(data.wicket_value); tr.append(td_value).append(td_wicket);
 
-      last_tr_from_first = $('div.history_calls #first.items table tr').last().remove();
       $('div.history_calls #first.items .label').after(tr);
-      $('div.history_calls #second.items table tr').last().remove();
-      $('div.history_calls #second.items .label').after(last_tr_from_first);
+
+      if($('div.history_calls #first.items table tr').size() == 5 ){
+        last_tr_from_first = $('div.history_calls #first.items table tr').last().remove();
+        $('div.history_calls #second.items .label').after(last_tr_from_first);
+        if($('div.history_calls #second.items table tr').size() == 5){
+          $('div.history_calls #second.items table tr').last().remove();
+        }
+      };
 
       //Plays songs of notification
       $.playAudio("/audios/beep_alert.ogg");

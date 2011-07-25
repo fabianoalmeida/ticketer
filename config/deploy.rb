34931c40d0400b3f57caac83e64f76e950e538bf
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-07-25 09:43:17 -0300

    Adding Redis  and Juggernaut Reciepes to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,28 +4,81 @@ set :repository,  "https://github.com/fabianoalmeida/ticketer.git"
 
 set :scm, :git
 set :deploy_to, "/var/www/#{application}"
+set :local_repository, "."
 
 set :user , 'ticketer'
 set :use_sudo, false
 server "ticketer.hsi.scmba.com.br", :app, :web, :db, :primary => true
+default_run_options[:pty] = true
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
-
- #If you are using Passenger mod_rails uncomment this:
-
- namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
- end
-
+#RVM RECIPE
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 set :rvm_bin_path, "$HOME/bin"
 set :rvm_ruby_string, '1.9.2-p180@ticketer'
 set :rvm_type, :user
+
+#BUNDLE RECIPE
 require "bundler/capistrano"
-#implement recipe for juggernaut 2
+
+#REDIS RECIPE
+set :redis_init_path, "/etc/init.d/redis"
+namespace :redis do
+  desc "Starting the redis Server"
+  task :start, :roles => :app  do
+    run "#{try_sudo} #{redis_init_path} start"
+  end
+
+  desc "Stoping the redis Server"
+  task :stop, :roles => :app  do
+    run "#{try_sudo} #{redis_init_path} stop"
+  end
+
+  desc "Restarting the redis Server"
+  task :restart, :roles => :app  do
+    run "#{try_sudo} #{redis_init_path} restart"
+  end
+end
+
+#JUGGERNAUT RECIPE
+namespace :juggernaut do
+
+  desc "Start the juggernaut Server"
+  task :start, :roles => :app  do
+    run "#{try_sudo} start juggernaut"
+  end
+
+  desc "Start the juggernaut Server"
+  task :stop , :roles => :app do
+    run "#{try_sudo} stop juggernaut"
+  end
+
+  desc "Start the juggernaut Server"
+  task :restart, :roles => :app  do
+    juggernaut.stop
+    juggernaut.start
+  end
+end
+
+#DEPLOY RECIPE
+set :nginx_init_path, "/etc/init.d/nginx"
+namespace :deploy do
+  task :start, :roles => :app do
+    run "#{try_sudo} #{nginx_init_path} start"
+    redis.start
+    juggernaut.strt
+  end
+
+  task :stop, :roles => :app do
+    run "#{try_sudo} #{nginx_init_path} stop"
+    redis.stop
+    juggernaut.stop
+  end
+
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+    redis.restart
+    juggernaut.restart
+  end
+end
+

2dc57ba009e37d38d88e7e0e3c3e74f7c711fe11
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-07-26 14:18:12 -0300

    Adding sequence to start with 1 instead 10000

--- lib/ticketer.rb
+++ lib/ticketer.rb
@@ -1,7 +1,10 @@
 module Ticketer
   class ActiveRecord::Base
     def self.today
-      where(:created_at => Date.today.midnight...Date.tomorrow.midnight) 
+      where(:created_at => Date.today.midnight...Date.tomorrow.midnight)
     end
+  end
+  ActiveRecord::ConnectionAdapters::OracleEnhancedAdapter.class_eval do
+    self.default_sequence_start_value = 1
   end
 end

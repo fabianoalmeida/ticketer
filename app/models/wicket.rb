0d2237c2281ed3efe3851eacda7182f8a6b7652c
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-07-26 10:14:39 -0300

    Elminate the ambiguos column issues on order by

--- app/models/wicket.rb
+++ app/models/wicket.rb
@@ -18,17 +18,17 @@ class Wicket < ActiveRecord::Base
     :length => { :in => 1..80 },
     :uniqueness => { :case_sensitive => false, :scope => :place_id },
     :allow_blank => false
-  
+
   def attended_tickets
-    tickets.where(:status_ticket_id => StatusTicket.attended.id).order("updated_at DESC")
+    tickets.where(:status_ticket_id => StatusTicket.attended.id).order("ticketes.updated_at DESC")
   end
-  
+
   def called_tickets
-    tickets.where(:status_ticket_id => StatusTicket.called.id).order("updated_at DESC")
+    tickets.where(:status_ticket_id => StatusTicket.called.id).order("ticketes.updated_at DESC")
   end
-  
+
   def pending_tickets
-    tickets.where(:status_ticket_id => StatusTicket.pending.id).order("updated_at DESC")
+    tickets.where(:status_ticket_id => StatusTicket.pending.id).order("ticketes.updated_at DESC")
   end
 
   def guidance_left?

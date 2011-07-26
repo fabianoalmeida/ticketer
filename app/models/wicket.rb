3122a836ea0e73b3cfb21d7bc9d0d0c70891a910
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-07-26 10:29:14 -0300

    Misspelled tickets on wicket scopes

--- app/models/wicket.rb
+++ app/models/wicket.rb
@@ -20,15 +20,15 @@ class Wicket < ActiveRecord::Base
     :allow_blank => false
 
   def attended_tickets
-    tickets.where(:status_ticket_id => StatusTicket.attended.id).order("ticketes.updated_at DESC")
+    tickets.where(:status_ticket_id => StatusTicket.attended.id).order("tickets.updated_at DESC")
   end
 
   def called_tickets
-    tickets.where(:status_ticket_id => StatusTicket.called.id).order("ticketes.updated_at DESC")
+    tickets.where(:status_ticket_id => StatusTicket.called.id).order("tickets.updated_at DESC")
   end
 
   def pending_tickets
-    tickets.where(:status_ticket_id => StatusTicket.pending.id).order("ticketes.updated_at DESC")
+    tickets.where(:status_ticket_id => StatusTicket.pending.id).order("tickets.updated_at DESC")
   end
 
   def guidance_left?

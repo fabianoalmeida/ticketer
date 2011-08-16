35d436331dd74728d9f4e92390abf96117ddeef8
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-08-16 15:29:42 -0300

    Modified query to return formated date and add missing file to tipsy.js

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,5 @@
 class Report
   def self.tickets_per_day(start_date, end_date)
-    Ticket.select("trunc(created_at) as date_local, count(id) as count_id").where(:created_at => start_date..end_date).group("trunc(created_at)").order("trunc(created_at) DESC")
+    Ticket.select("to_char(trunc(created_at), 'dd/MM/yyyy' ) as date_local, count(id) as count_id").where(:created_at => start_date.midnight..end_date.tomorrow.midnight).group("trunc(created_at)").order("trunc(created_at) DESC")
   end
 end

f35180b7e5714074d1b344907b7cd366739c3dff
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-08-16 17:10:55 -0300

    Better indentit

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,8 @@
 class Report
   def self.tickets_per_day(start_date, end_date)
-    Ticket.select("to_char(trunc(created_at), 'dd/MM/yyyy' ) as date_local, count(id) as count_id").where(:created_at => start_date.midnight..end_date.tomorrow.midnight).group("trunc(created_at)").order("trunc(created_at) DESC")
+    Ticket.select("to_char(trunc(created_at), 'dd/MM/yyyy' ) as date_local, count(id) as count_id")
+          .where(:created_at => start_date.midnight..end_date.tomorrow.midnight)
+          .group("trunc(created_at)").order("trunc(created_at) DESC")
+
   end
 end

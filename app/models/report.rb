ec825d146848a38b87ae2cc360c5fb6c18f63adc
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-08-16 12:26:39 -0300

    changed query

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,5 +1,5 @@
 class Report
   def self.tickets_per_day(start_date, end_date)
-   Ticket.where(:created_at => start_date..end_date).count(:group => ["TO_CHAR(created_at, 'dd/MM/yyyy')"])
+    Ticket.select("trunc(created_at) as date_local, count(id) as count_id").where(:created_at => start_date..end_date).group("trunc(created_at)").order("trunc(created_at) DESC")
   end
 end

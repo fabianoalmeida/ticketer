4e9984c34ba768d0129c53a2397f76e3d9fd7124
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-08-17 11:05:32 -0300

    Fixing return when there's no results

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,19 +4,19 @@ class ReportsController < ApplicationController
 
   def tickets_per_day
     respond_to do |format|
-      unless params[:start_date].blank? && params[:end_date].blank?
+      unless(params[:start_date].blank? && params[:end_date].blank?)
         @tickets_per_day = Report.tickets_per_day(
           params[:start_date].to_date,
           params[:end_date].to_date
         )
+        debugger
         unless @tickets_per_day.empty?
           format.html
           format.json { render :json => @tickets_per_day }
         end
-      else
-        format.html {render :notice => I18n.t('application.no_results')}
-        format.json {render :json => nil}
       end
+      format.html {render :notice => I18n.t('application.no_results')}
+      format.json {render :json => nil}
     end
   end
 

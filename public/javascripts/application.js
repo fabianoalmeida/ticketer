707d257b0ed2d764db599dd3de9d93e3f9e55579
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-08-17 15:30:29 -0300

    Adding Translate to pt-BR and double calendar

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,10 +7,23 @@
 })($);
 
 $(function(){
-  $('input.date').datepicker({
-                    autoSize : true,
-                    dateFormat : 'yy-mm-dd',
-                    changeYear : true,
-                    changeMonth : true
+  $.datepicker.setDefaults($.datepicker.regional['pt-BR']);
+
+  var dates = $( "input.date.from,  input.date.to" ).datepicker({
+      dateFormat : 'dd-mm-yy',
+      showButtonPanel: true,
+      autoSize : true,
+      numberOfMonths: 2,
+      onSelect: function( selectedDate ) {
+	      var option = this.id == 'start_date' ? "minDate" : "maxDate",
+		      instance = $( this ).data( "datepicker" ),
+		      date = $.datepicker.parseDate(
+			      instance.settings.dateFormat ||
+			      $.datepicker._defaults.dateFormat,
+			      selectedDate, instance.settings );
+	      dates.not( this ).datepicker( "option", option, date );
+      }
   });
+
+
 })

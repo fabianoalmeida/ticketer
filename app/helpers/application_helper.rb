a6f9d74122ff8bd54e9513a97eec2450de5dcc88
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-09-27 14:50:52 -0300

    added new helpers to attendaces by wicket per month report

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  def unabbreviated_month(month)
+    (I18n.t('date.month_names'))[month]
+  end
+  
+  def unabbreviated_month_year(month_year)
+    arr       = month_year.split('/')
+    month_int = arr[0].to_i
+    year_int  = arr[1].to_i
+    "#{(I18n.t('date.month_names'))[month_int]} / #{year_int}"
+  end
+
 end

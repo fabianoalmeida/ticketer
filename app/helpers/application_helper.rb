5ad51edd45c7aa7f15f3dc24aa7e8e18d0a4f7f0
Vinícius Oliveira <oliveira.mvs@gmail.com>
2011-09-29 14:29:53 -0300

    Convert to string before try to split

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,7 +5,7 @@ module ApplicationHelper
   end
   
   def unabbreviated_month_year(month_year)
-    arr       = month_year.split('/')
+    arr       = (month_year.to_s).split('/')
     month_int = arr[0].to_i
     year_int  = arr[1].to_i
     "#{(I18n.t('date.month_names'))[month_int]} / #{year_int}"

a4829267326076999f4e203d1fe8cd9fc294312f
Fabiano Almeida <fabianoalmeida@gmail.com>
2011-12-27 16:23:00 -0300

    Created some metaprogramming and added the second_level validation

--- app/models/wicket.rb
+++ app/models/wicket.rb
@@ -12,24 +12,24 @@ class Wicket < ActiveRecord::Base
   has_many :tickets, :through => :call_histories, :uniq => :true
 
   validates :value, :user, :status, :place, :ticket_type_group, :presence => true
-  validates_inclusion_of :guidance, :priority, :in => [true, false]
+  validates_inclusion_of :guidance, :priority, :second_level, :in => [true, false]
 
   validates :value,
     :length => { :in => 1..80 },
     :uniqueness => { :case_sensitive => false, :scope => :place_id },
     :allow_blank => false
 
-  def attended_tickets
-    tickets.where(:status_ticket_id => StatusTicket.attended.id).order("tickets.updated_at DESC")
-  end
+  def self.tickets_by_status(*args) 
+    args.each do |status| 
+      instance_eval do 
+        define_method "#{status}_tickets" do
+          tickets.where(:status_ticket_id => StatusTicket.id_for("#{status.to_s}").id).order("tickets.updated_at DESC")
+        end 
+      end
+    end
+  end    
 
-  def called_tickets
-    tickets.where(:status_ticket_id => StatusTicket.called.id).order("tickets.updated_at DESC")
-  end
-
-  def pending_tickets
-    tickets.where(:status_ticket_id => StatusTicket.pending.id).order("tickets.updated_at DESC")
-  end
+  tickets_by_status :attended, :called, :pending, :examed
 
   def guidance_left?
     return guidance ? false : true

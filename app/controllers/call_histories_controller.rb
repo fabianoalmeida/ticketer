f7aa2233245b5a97bccb0c417c88352797453872
Fabiano Almeida <fabianoalmeida@gmail.com>
2012-04-12 15:44:16 -0300

    Changed to show all registers for current date and modified the layout

--- app/controllers/call_histories_controller.rb
+++ app/controllers/call_histories_controller.rb
@@ -2,7 +2,7 @@ class CallHistoriesController < ApplicationController
   # GET /call_histories
   # GET /call_histories.json
   def index
-    @call_histories = CallHistory.all
+    @call_histories = CallHistory.today.order( "created_at DESC" )
 
     respond_to do |format|
       format.html # index.html.erb

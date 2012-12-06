28a9564e2d5e279ef03fdddf04659e5f9ce871c9
Fabiano Almeida <fabianoalmeida@gmail.com>
2012-12-06 10:58:26 -0200

    Fixed showing local time after create a new ticket

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -47,7 +47,7 @@ class TicketsController < ApplicationController
     respond_to do |format|
       if @ticket.save
         TicketPrint.print( @ticket )
-        @ticket[:created_at_formatted] = I18n.localize(@ticket.created_at, :format => :default)
+        @ticket[:created_at_formatted] = I18n.localize(@ticket.created_at.localtime, :format => :default)
         @ticket[:ticket_type_value] = @ticket.ticket_type.value.to_s
         @ticket[:place_value] = @ticket.place.value.to_s
         format.html { redirect_to(@ticket, :notice => 'Ticket was successfully created.') }
